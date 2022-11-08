1d8dfcf64935175216d6798af40c3a7abb4e6814
nodb <scnoh02@naver.com>
2022-11-08 23:06:47 +0900

    Update quarter /분기

--- quarter.py
+++ quarter.py
@@ -1,35 +1,27 @@
 from urllib.parse import quote
-
 import requests
-import json
-import random
-import time
+def quarter(date):
+    encoding = quote(date)
+    header = {'laftel': 'TeJava'}
 
-boongi = input()  # 분기를 입력받는 코드
-encoding = quote(boongi)
+    laftel_API = 'https://laftel.net/api/search/v1/discover/?years=' + str(encoding)
 
-header = {'laftel': 'TeJava'}
+    result = []
 
-laftel_API = 'https://laftel.net/api/search/v1/discover/?years=' + str(encoding)
+    while (True):
+        response = requests.get(url=laftel_API, headers=header)
+        json = response.json()
+        results = json["results"]
 
-next = ""
-print("==============================")
 
-while (True):
-    response = requests.get(url=laftel_API, headers=header)
+        for i, x in enumerate(results, 1):
+            result.append({'name': x["name"], 'id': x["id"]})
 
-    json = response.json()
+        next = json["next"]
+        if (next is not None):
+            laftel_API = next
+        else:
+            break
+        # time.sleep(random.randint(1, 2))
 
-    results = json["results"]
-
-    for x in results:
-        print(x["name"])
-
-    next = json["next"]
-    if (next is not None):
-        laftel_API = next
-    else:
-        break
-    time.sleep(random.randint(1, 2))
-
-print("==============================")
+    return result
